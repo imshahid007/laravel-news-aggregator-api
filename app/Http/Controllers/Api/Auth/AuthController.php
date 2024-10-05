bb75c4e9b3c867e6f21448fca8225404bec4d7af
= <=>
2024-10-06 01:34:35 +0200

    Added Auth files

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+use Illuminate\Validation\ValidationException;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password as Password;
+use Illuminate\Validation\Rules\Password as PasswordRules;
+use Illuminate\Support\Str;
+
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', PasswordRules::defaults()],
+            'device_name' => ['required'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken($request->device_name)->plainTextToken;
+        //
+        return response()->json([
+            'message' => 'User created successfully',
+            'token'   => $token,
+            'user'    => $user,
+
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+            'device_name' => 'required',
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details',
+            ], 401);
+        }
+
+        $user = auth()->user();
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token,
+            'user' => $user,
+
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        // Revoke the token that was used to authenticate the current request
+		// $request->user()->currentAccessToken()->delete();
+		$request->user->tokens()->delete(); // To revoke all tokens (logout from all devices)
+
+        return response()->json([
+            'message' => 'Logged out successfully',
+        ]);
+    }
+
+
+    public function getAuthenticatedUser(Request $request) {
+		return $request->user();
+	}
+
+	public function sendPasswordResetLinkEmail(Request $request) {
+		$request->validate(['email' => 'required|email']);
+
+		$status = Password::sendResetLink(
+			$request->only('email')
+		);
+
+		if($status === Password::RESET_LINK_SENT) {
+			return response()->json(['message' => __($status)], 200);
+		} else {
+			throw ValidationException::withMessages([
+				'email' => __($status)
+			]);
+		}
+	}
+
+	public function resetPassword(Request $request) {
+		$request->validate([
+			'token' => 'required',
+			'email' => 'required|email',
+			'password' => 'required|min:8|confirmed',
+		]);
+
+		$status = Password::reset(
+			$request->only('email', 'password', 'password_confirmation', 'token'),
+			function ($user, $password) use ($request) {
+				$user->forceFill([
+					'password' => Hash::make($password)
+				])->setRememberToken(Str::random(60));
+
+				$user->save();
+
+			}
+		);
+
+		if($status == Password::PASSWORD_RESET) {
+			return response()->json(['message' => __($status)], 200);
+		} else {
+			throw ValidationException::withMessages([
+				'email' => __($status)
+			]);
+		}
+	}
+}
---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\AuthController;
+//
+Route::prefix('auth')->group(function () {
+    // Group routes by middleware
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    // Password reset routes
+    Route::post('password/email', [AuthController::class, 'sendPasswordResetLinkEmail']);
+    Route::post('password/reset', [AuthController::class, 'resetPassword'])->name('password.reset');;
+
+    // Authenticated user routes
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('user', [AuthController::class, 'getAuthenticatedUser']);
+    });
+});
